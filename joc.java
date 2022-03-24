56b9e4017c0c3be8e116c167aaae2f3648a1373c
Pau4Serra <2001pauserra@gmail.com>
2022-03-24 16:57:58 +0100

    Primer commit

--- joc.java
+++ joc.java
@@ -27,21 +27,9 @@ public class joc {
 				}
 				
 				break;
-			
+				
 			case 2:
-				if (option == 2 ) {
-					
-					int [] normes = definirNormes();
-					System.out.println();
-					System.out.println("Normes aplicades: ");
-					System.out.println(normes[0] + "" + normes[1] + "/" + normes[2]);
-					System.out.println();
-				}
-				
-				break;
-				
-			case 3:
-				if (option == 3) {
+				if (option == 2) {
 					
 					generarTauler(board);
 					posicionamentManual(board);
@@ -49,8 +37,8 @@ public class joc {
 					break;
 				}
 				
-			case 4:
-				if (option == 4) {
+			case 3:
+				if (option == 3) {
 
 					generarTauler(board);
 					posicionamentAuto(board);
@@ -59,8 +47,8 @@ public class joc {
 				}
 				
 				
-			case 5:
-				if (option == 5) {
+			case 4:
+				if (option == 4) {
 					
 					System.out.println();
 					imprimirTauler(board);
@@ -69,10 +57,18 @@ public class joc {
 				
 				}	
 				
-			case 6:
-				if (option == 6) {
+			case 5:
+				if (option == 5) {
 					
+					int [] normes = definirNormes();
+					System.out.println();
+					System.out.println("Normes aplicades: ");
+					System.out.println(normes[0] + "" + normes[1] + "/" + normes[2]);
+					System.out.println();
 					
+					game(board, normes);
+				
+				break;	
 					
 				}
 			
@@ -85,7 +81,141 @@ public class joc {
 	}
 	
 	private void game(char [][] board, int [] normes) {
-		
+		int contador = 0;
+		int iteracions = 0;
+		char [][] newBoard = null;
+		newBoard = board;
+		System.out.println("Quantes iteracions voldràs que faci el joc? ");
+		iteracions = e.nextInt();
+		for (int s = 0; s < iteracions; s++){
+			for (int y = 0; y < board.length; y++) {
+				for (int x = 0; x < board[y].length; x++) {
+					if (board[y][x] == '■') {
+						contador = 0;
+						if (board[y].length > (y+1)) {
+							if (board[y+1][x] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (board[y].length > (y+1) && board.length > (x+1)) {
+							if (board[y+1][x+1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (board.length > (x+1)) {
+							if (board[y][x+1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (y-1) && board.length > (x+1)) {
+							if (board[y-1][x+1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (y-1)) {
+							if (board[y-1][x] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (y-1) && 0 <= (x-1)) {
+							if (board[y-1][x-1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (x-1)) {
+							if (board[y][x-1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (board[y].length > (y+1) && 0 <= (x-1)) {
+							if (board[y+1][x-1] == '■') {
+								contador++;
+							}
+						}
+						
+						if (contador >= normes[0] && contador <= normes[1]) {
+							newBoard[y][x] = '■';
+						} else {
+							newBoard[y][x] = '□';
+						}
+						
+					} else {
+						contador = 0;
+						if (board[y].length > (y+1)) {
+							if (board[y+1][x] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (board[y].length > (y+1) && board.length > (x+1)) {
+							if (board[y+1][x+1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (board.length > (x+1)) {
+							if (board[y][x+1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (y-1) && board.length > (x+1)) {
+							if (board[y-1][x+1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (y-1)) {
+							if (board[y-1][x] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (y-1) && 0 <= (x-1)) {
+							if (board[y-1][x-1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (0 <= (x-1)) {
+							if (board[y][x-1] == '■') {
+								contador++;
+							}
+						} 
+						
+						if (board[y].length > (y+1) && 0 <= (x-1)) {
+							if (board[y+1][x-1] == '■') {
+								contador++;
+							}
+						}
+						
+						if (contador == normes[2]) {
+							newBoard[y][x] = '■';
+						} else {
+							newBoard[y][x] = '□';
+						}
+					}
+				}
+			}
+			
+			char next;
+			System.out.println("NEXT? (Y/N) ");
+			next = e.next().toLowerCase().charAt(0);
+			if (next == 'y') {
+				imprimirTauler(newBoard);
+			}
+			
+			else if (next == 'n') {
+				break;
+			}
+		} 
 	}
 	
 	private int length() {
@@ -123,11 +253,10 @@ public class joc {
 		System.out.println("BENVINGUT AL JOC DE LA VIDA");
 		System.out.println("Escull que vols fer: ");
 		System.out.println("1 - Definir tauler");
-		System.out.println("2 - Definir regles");
-		System.out.println("3 - Introduïr manualment les cèl·lules");
-		System.out.println("4 - Introduïr automàticament les cèl·lules");
-		System.out.println("5 - Imprimir tauler");
-		System.out.println("6 - Iniciar el joc");
+		System.out.println("2 - Introduïr manualment les cèl·lules");
+		System.out.println("3 - Introduïr automàticament les cèl·lules");
+		System.out.println("4 - Imprimir tauler");
+		System.out.println("5 - Iniciar el joc");
 		System.out.println("0 - Sortir");
 		System.out.println();
 		System.out.print("> ");
@@ -136,12 +265,14 @@ public class joc {
 		return menu;
 	}
 	
-	private void generarTauler(char [][] board) {
+	private char [][] generarTauler(char [][] board) {
 		for (int y = 0; y < board.length; y++) {
 			for (int x = 0; x < board[y].length; x++) {
 				board[y][x] = '□';
 			}
 		}
+		
+		return board;
 	}
 	
 	private void imprimirTauler(char [][] board) {
